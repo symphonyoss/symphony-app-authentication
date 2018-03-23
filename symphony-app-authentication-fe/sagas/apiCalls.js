e4af8512cdcfcd813870caf36653da8221d5cf4a
Cassiano Repache <crepache@daitangroup.com>
2018-03-23 09:57:19 -0300

    Add apiCalls

---
+++ symphony-app-authentication-fe/sagas/apiCalls.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const port = window.location.port;
+const hostPort = (!port || port.length === 0) ? `` : `:${port}`;
+const baseUrl = `${window.location.protocol}//${window.location.hostname}${hostPort}/integration`;
+
+const baseAuthenticationUrl;;
+
+export default class ApiCalls {
+
+    constructor(baseAuthenticationUrl) {
+        this.baseAuthenticationUrl = `${baseAuthenticationUrl}/v1/application/jwt`
+    }
+
+    authenticateApp(podId, appId) {
+        // use appId 
+        const url = `${baseAuthenticationUrl}/authenticate`;
+        const payload = {
+            podId
+        }
+
+        return axios.post(url, payload);
+    }
+
+    validateTokens(applicationToken, symphonyToken, appId) {
+        // use appId 
+        const url = `${baseAuthenticationUrl}/tokens/validate`;
+        const payload = {
+            applicationToken,
+            symphonyToken
+        };
+
+        return axios.post(url, payload);
+    }
+
+    validateJwt(jwt) {
+        const url = `${baseAuthenticationUrl}/validate`;
+        const payload = {
+            jwt
+        };
+
+        return axios.post(url, payload);
+    }
+}
