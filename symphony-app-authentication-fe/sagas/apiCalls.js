1b24e89629d91d2f84f9265e64c7e84e446be9cb
Cassiano Repache <crepache@daitangroup.com>
2018-03-23 16:41:15 -0300

    Removing spaces

--- symphony-app-authentication-fe/sagas/apiCalls.js
+++ symphony-app-authentication-fe/sagas/apiCalls.js
@@ -3,38 +3,37 @@ import axios from 'axios';
 const baseAuthenticationUrl = "";
 
 export default class ApiCalls {
+  constructor(baseAuthenticationUrl) {
+    this.baseAuthenticationUrl = `${baseAuthenticationUrl}/v1/application/jwt`
+  }
 
-    constructor(baseAuthenticationUrl) {
-        this.baseAuthenticationUrl = `${baseAuthenticationUrl}/v1/application/jwt`
+  authenticateApp(podId, appId) {
+    // use appId 
+    const url = `${baseAuthenticationUrl}/authenticate`;
+    const payload = {
+        podId
     }
 
-    authenticateApp(podId, appId) {
-        // use appId 
-        const url = `${baseAuthenticationUrl}/authenticate`;
-        const payload = {
-            podId
-        }
+    return axios.post(url, payload);
+  }
 
-        return axios.post(url, payload);
-    }
+  validateTokens(applicationToken, symphonyToken, appId) {
+    // use appId 
+    const url = `${baseAuthenticationUrl}/tokens/validate`;
+    const payload = {
+        applicationToken,
+        symphonyToken
+    };
 
-    validateTokens(applicationToken, symphonyToken, appId) {
-        // use appId 
-        const url = `${baseAuthenticationUrl}/tokens/validate`;
-        const payload = {
-            applicationToken,
-            symphonyToken
-        };
+    return axios.post(url, payload);
+  }
 
-        return axios.post(url, payload);
-    }
+  validateJwt(jwt) {
+    const url = `${baseAuthenticationUrl}/validate`;
+    const payload = {
+        jwt
+    };
 
-    validateJwt(jwt) {
-        const url = `${baseAuthenticationUrl}/validate`;
-        const payload = {
-            jwt
-        };
-
-        return axios.post(url, payload);
-    }
+    return axios.post(url, payload);
+  }
 }
