c9197bac97836b4c4910874ef9d6fcac4f8ec451
Cassiano Repache <crepache@daitangroup.com>
2018-03-27 14:51:22 -0300

    Fix comment on my PR

--- symphony-app-authentication-fe/services/registerApplication.js
+++ symphony-app-authentication-fe/services/registerApplication.js
@@ -4,11 +4,10 @@
 * @return       SYMPHONY.remote.hello       returns a SYMPHONY remote hello service.
 */
 export const registerApplication = (config, appData) => {
-  const controllerName = `${config.appId}:controller`;
-
-  if (config.dependencies.indexOf('extended-user-info') == -1) {
-    config.dependencies.push('extended-user-info');
-  }
+  // The next 3 lines were added to insert extended-user-info as a dependency only if it don't alredy exists.
+  let dependencies = new Set(config.dependencies);
+  dependencies.add('extended-user-info');
+  config.dependencies = [... dependencies];
 
   return SYMPHONY.application.register(appData, config.dependencies, config.exportedDependencies);
 };
